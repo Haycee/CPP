c0d22466e58a1c570ce6ab4b4fd3994c0e59cbe4
Girardin Alex <alex@M1.local>
2022-08-03 01:07:57 +0200

    Small changes

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:42:20 by agirardi          #+#    #+#             */
-/*   Updated: 2022/06/30 10:42:06 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/03 01:07:21 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int main(int argc, char *argv[])
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i(1); i < argc; i++)
-		for (int j(0); j < (int)strlen(argv[i]); j++)
+
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
 			std::cout << (char)toupper(argv[i][j]);
 	std::cout << std::endl;
+
 	return (0);
 }
