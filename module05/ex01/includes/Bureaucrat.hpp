4afb4a65a34d8ca9ca15c37d85c81556cd6b7103
Alex Girardin <agirardi@z4r1p7.42lyon.fr>
2022-08-10 10:57:24 +0200

    Fix const variable name

--- module05/ex01/includes/Bureaucrat.hpp
+++ module05/ex01/includes/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 18:51:52 by agirardi          #+#    #+#             */
-/*   Updated: 2022/07/28 23:45:18 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/10 10:55:56 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,12 +48,12 @@ class Bureaucrat
 				}
 		};
 
-		Bureaucrat &	operator=(Bureaucrat const & rhs);
-
 	private:
 
-		std::string	_name;
-		int			_grade;
+		const std::string	_name;
+		int					_grade;
+
+		Bureaucrat &	operator=(Bureaucrat const & rhs);
 };
 
 std::ostream &	operator<<(std::ostream & o, Bureaucrat const & rhs);
