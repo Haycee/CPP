018a0e364b9810a166c49c293ab13a629518406e
Alex <alex@m1.home>
2022-08-23 19:33:26 +0200

    Small changes

--- module00/ex01/includes/Contact.hpp
+++ module00/ex01/includes/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 10:50:56 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/08 15:21:17 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/23 19:32:19 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ class Contact
 		void	printRecapInfo(void) const;
 
 	private:
+
 		int			_index;
 		std::string	_firstName;
 		std::string	_lastName;
 		std::string	_nickname;
 		std::string	_phoneNumber;
 		std::string	_darkestSecret;
+
 		std::string	_formatStr(std::string str) const;
 };
--- module00/ex01/includes/Phonebook.hpp
+++ module00/ex01/includes/Phonebook.hpp
@@ -6,12 +6,11 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 10:50:56 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/06 21:49:48 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/23 19:33:06 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 #include "main.hpp"
 
 class Phonebook
@@ -25,8 +24,10 @@ class Phonebook
 		int	displayContact(void);
 
 	private:
+
 		Contact	_contact[8];
 		int		_contactCount;
 		int		_contactIndex;
+
 		int		_promptContactIndex(void);
 };
