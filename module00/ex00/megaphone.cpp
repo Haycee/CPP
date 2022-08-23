ff65ca05509d096e9961d65c8c47282e7f4155cd
Alex <alex@m1.home>
2022-08-23 20:34:43 +0200

    Quick fixes

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,11 +6,13 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:42:20 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/23 19:28:26 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/23 20:33:50 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
+#include <cctype>
 
 int main(int argc, char *argv[])
 {
@@ -18,8 +20,8 @@ int main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 	for (int i = 1; i < argc; i++)
-		for (int j = 0; j < (int)strlen(argv[i]); j++)
-			std::cout << (char)toupper(argv[i][j]);
+		for (size_t j = 0; j < std::strlen(argv[i]); j++)
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
 	std::cout << std::endl;
 	return (0);
 }
