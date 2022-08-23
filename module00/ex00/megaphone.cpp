450a51e9f35794bd5933252eb156b7e8e8f04b04
Alex <alex@m1.home>
2022-08-23 19:28:59 +0200

    Small changes

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:42:20 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/03 01:07:21 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/23 19:28:26 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ int main(int argc, char *argv[])
 		for (int j = 0; j < (int)strlen(argv[i]); j++)
 			std::cout << (char)toupper(argv[i][j]);
 	std::cout << std::endl;
-
 	return (0);
 }
