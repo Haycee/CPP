0bc329d00378fa2fbb02c28d1a28707ff2b6071c
Alex Girardin <agirardi@z4r2p1.42lyon.fr>
2022-08-24 10:49:14 +0200

    Norme

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 20:54:13 by agirardi          #+#    #+#             */
-/*   Updated: 2022/07/06 21:07:18 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 10:48:23 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ int	main(void)
 	std::cout << str << std::endl;
 	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
+
 	return (0);
 }
