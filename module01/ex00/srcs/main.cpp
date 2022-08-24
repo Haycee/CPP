b2fb7d4617689e9f7c8b717e8a7aae913e2eed39
Alex Girardin <agirardi@z4r2p1.42lyon.fr>
2022-08-24 14:08:37 +0200

    Norme

--- module01/ex00/srcs/main.cpp
+++ module01/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 17:22:19 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/24 10:41:21 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 14:08:25 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 int	main(void)
 {
 	Zombie	*foo = newZombie("foo");
+
 	foo->announce();
+	delete foo;
 
 	randomChump("bar");
 
-	delete foo;
 	return (0);
 }
