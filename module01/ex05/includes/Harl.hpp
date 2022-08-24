173e85e976a82918d956573fd740c6b256e95e83
Alex Girardin <agirardi@z4r2p1.42lyon.fr>
2022-08-24 11:14:16 +0200

    Norme

--- module01/ex05/includes/Harl.hpp
+++ module01/ex05/includes/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:45:14 by agirardi          #+#    #+#             */
-/*   Updated: 2022/07/09 17:56:55 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 11:09:00 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ class Harl
 		Harl(void);
 		~Harl(void);
 
-		void complain(std::string level);
+		void	complain(std::string level);
 
 	private:
 
-		void _debug(void);
-		void _info(void);
-		void _warning(void);
-		void _error(void);
+		void	_debug(void);
+		void	_info(void);
+		void	_warning(void);
+		void	_error(void);
 };
--- module01/ex05/srcs/main.cpp
+++ module01/ex05/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:44:01 by agirardi          #+#    #+#             */
-/*   Updated: 2022/07/09 17:56:50 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 11:07:32 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(void)
 {
-	Harl harl = Harl();
+	Harl	harl = Harl();
+
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
 	harl.complain("ERROR");
+
 	return (0);
 }
