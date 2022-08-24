d3f0298e613a700db137eebf46d5ba3b3f66f8e4
Alex Girardin <agirardi@z4r2p1.42lyon.fr>
2022-08-24 10:46:07 +0200

    Norme

--- module01/ex01/srcs/main.cpp
+++ module01/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:44:01 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/08 15:14:24 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 10:45:37 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(void)
 {
-	Zombie *horde = zombieHorde(5, "foo");
+	Zombie	*horde = zombieHorde(5, "foo");
 	for (int i = 0; i < 5; i++)
 		horde[i].announce();
 
--- module01/ex01/srcs/zombieHorde.cpp
+++ module01/ex01/srcs/zombieHorde.cpp
@@ -6,13 +6,13 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:45:46 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/08 15:13:31 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/08/24 10:44:25 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-Zombie* zombieHorde(int N, std::string name)
+Zombie*	zombieHorde(int N, std::string name)
 {
 	Zombie *horde = new Zombie[N];
 
