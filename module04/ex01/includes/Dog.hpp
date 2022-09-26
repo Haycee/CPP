cf078c30eae0515680b844b3c37932aa8058deb0
Alex Girardin <agirardi@z1r12p1.42lyon.fr>
2022-09-26 11:10:20 +0200

    Fix Dog destructor

--- module04/ex01/includes/Dog.hpp
+++ module04/ex01/includes/Dog.hpp
@@ -6,13 +6,13 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 18:51:54 by agirardi          #+#    #+#             */
-/*   Updated: 2022/09/26 10:52:41 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/09/26 11:09:11 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "main.hpp"
-
+	
 class Dog : public Animal
 {
 	public:
