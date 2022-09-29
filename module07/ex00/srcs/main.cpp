14046a6bd043e026177c306d8204bc5fa5ce6a9d
Alex Girardin <agirardi@z4r1p7.42lyon.fr>
2022-09-29 13:48:34 +0200

    Added more tests

--- module07/ex00/srcs/main.cpp
+++ module07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:06:58 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/08 10:11:53 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/09/29 13:47:04 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,20 @@ int main(void)
 	int	a = 2;
 	int	b = 3;
 
+	std::cout << "Before Swap : a = " << a << ", b = " << b << std::endl;
 	::swap<int>( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "After Swap  : a = " << a << ", b = " << b << std::endl << std::endl;
 	std::cout << "min( a, b ) = " << ::min<int>( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max<int>( a, b ) << std::endl;
 
+	std::cout << std::endl << std::endl << std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+
+	std::cout << "Before Swap : c = " << c << ", d = " << d << std::endl;
 	::swap<std::string>(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "After Swap  : c = " << c << ", d = " << d << std::endl << std::endl;
 	std::cout << "min( c, d ) = " << ::min<std::string>( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max<std::string>( c, d ) << std::endl;
 
