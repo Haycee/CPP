3c6410dd0b606add12ae6d60636bdcaf1049f294
Alex Girardin <agirardi@z4r1p7.42lyon.fr>
2022-09-29 11:13:42 +0200

    Removed comments

--- module06/ex01/srcs/main.cpp
+++ module06/ex01/srcs/main.cpp
@@ -6,17 +6,11 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:06:58 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/02 17:19:02 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/09/29 11:12:25 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
-
-/*
-	struct forme canonique ?
-	https://stackoverflow.com/questions/1845482/what-is-uintptr-t-data-type/1846648#1846648
-	https://www.oreilly.com/library/view/understanding-and-using/9781449344535/ch01.html
-*/
 
 uintptr_t	serialize(Data* ptr);
 Data*		deserialize(uintptr_t raw);
