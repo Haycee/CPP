8d7e740d69e8891109ccb69bd1a17dfed7d1f90e
Haycee <95182577+Haycee@users.noreply.github.com>
2022-10-01 02:01:56 +0200

    Fix iter function

--- module07/ex01/includes/main.hpp
+++ module07/ex01/includes/main.hpp
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:08:12 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/03 18:28:52 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/10/01 01:57:02 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	print(T & a)
 	std::cout << a << std::endl;
 }
 
-template<typename T, typename F>
-void	iter(T array[], int size, F f)
+template<typename T>
+void	iter(T array[], int size, void f(T & a))
 {
 	for (int i = 0; i < size; i++)
 	{
--- module07/ex01/srcs/main.cpp
+++ module07/ex01/srcs/main.cpp
@@ -6,26 +6,28 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:06:58 by agirardi          #+#    #+#             */
-/*   Updated: 2022/08/03 18:32:01 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/10/01 01:57:04 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-/*
-	https://stackoverflow.com/questions/1282914/pass-pointer-to-template-function-as-function-argument
-*/
-
 int main(void)
 {
+	std::cout << std::endl << "====== TEST INT ======" << std::endl << std::endl;
+
 	int	intArray[5] = {1, 2, 3, 4, 5};
-	iter<int>(intArray, 5, print<int>);
+	iter(intArray, 5, print<int>);
+
+	std::cout << std::endl << "====== TEST DOUBLE ======" << std::endl << std::endl;
 
 	double	doubleArray[2] = {1.2, 3.4};
-	iter<double>(doubleArray, 2, print<double>);
+	iter(doubleArray, 2, print<double>);
+
+	std::cout << std::endl << "====== TEST STRING ======" << std::endl << std::endl;
 
 	std::string strArray[3] = {"AB", "CD", "EF"};
-	iter<std::string>(strArray, 3, print<std::string>);
+	iter(strArray, 3, print<std::string>);
 
 	return (0);
 }
